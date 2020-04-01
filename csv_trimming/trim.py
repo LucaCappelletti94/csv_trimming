e850f17e63c3735ba8ea384e5b2fbeb04e4d9182
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-04-01 16:27:30 +0200

    Trying to patch travis error

--- csv_trimming/trim.py
+++ csv_trimming/trim.py
@@ -16,7 +16,7 @@ def mask_edges(mask: np.ndarray):
         if not val:
             break
         left += 1
-    for val in np.flip(mask):
+    for val in np.flip(mask, axis=0):
         if not val:
             break
         right += 1
@@ -28,7 +28,7 @@ def _trim_padding(csv: pd.DataFrame) -> pd.DataFrame:
     nan_mask = is_na(csv)
     rows_threshold = np.logical_not(nan_mask).sum(axis=1).mean()/2
     rows_mask = mask_edges(np.logical_not(
-        nan_mask).sum(axis=1).values < rows_threshold)
+        nan_mask).sum(axis=1) < rows_threshold)
     columns_mask = mask_edges(nan_mask.all(axis=0).values)
     return csv[~rows_mask][csv.columns[~columns_mask]]
 
