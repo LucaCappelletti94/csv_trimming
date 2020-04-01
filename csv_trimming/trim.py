5d20a79f3b9d214d67f42c3b3512d013ce09ca9f
LucaCappelletti94 <cappelletti.luca94@gmail.com>
2020-04-01 16:22:28 +0200

    Patching issue encountered in travis

--- csv_trimming/trim.py
+++ csv_trimming/trim.py
@@ -29,7 +29,7 @@ def _trim_padding(csv: pd.DataFrame) -> pd.DataFrame:
     rows_threshold = np.logical_not(nan_mask).sum(axis=1).mean()/2
     rows_mask = mask_edges(np.logical_not(
         nan_mask).sum(axis=1) < rows_threshold)
-    columns_mask = mask_edges(nan_mask.all(axis=0))
+    columns_mask = mask_edges(nan_mask.all(axis=0).values)
     return csv[~rows_mask][csv.columns[~columns_mask]]
 
 
